c49100c2f6d94bbc17e39f21bba5d6032fe79b7e
Paul Krohn <paul@daemonize.com>
2014-12-21 20:33:27 -0800

    Initial checkin.

---
+++ nexus/nexus.py
@@ -0,0 +1,86 @@
+import requests
+import hashlib
+from lxml import etree
+import os
+import sys
+
+
+class Nexus(object):
+    def __init__(self, nexus_base_url="", **kwargs):
+        print "hey! the base url arg is :%s:" % nexus_base_url
+        self.nexus_auth = False
+        # check passed-in args, and env, for nexus user/pass, else leave blank
+        if "user" in kwargs.keys() and "password" in kwargs.keys():
+            self.nexus_auth = True
+            self.nexus_user = kwargs["user"]
+            self.nexus_pass = kwargs["password"]
+
+        if nexus_base_url == "":
+            raise ValueError("missing required argument nexus_base_url")
+        else:
+            self.nexus_base_url = nexus_base_url
+
+        if "download_directory" in kwargs.keys():
+            self.download_directory = kwargs["download_directory"]
+        else:
+            self.download_directory = os.getcwd()
+
+    @staticmethod
+    def _hash_file(file_path):
+        sha1 = hashlib.sha1()
+        f = open(file_path, 'rb')
+        try:
+            sha1.update(f.read())
+        finally:
+            f.close()
+        return sha1.hexdigest()
+
+    def _resolver_url(self, group_id, artifact_id, version, repository, packaging):
+        return self.nexus_base_url + "/service/local/artifact/maven/resolve" + \
+            "?g=" + group_id + \
+            "&a=" + artifact_id + \
+            "&v=" + version + \
+            "&r=" + repository + \
+            "&p=" + packaging
+
+    def _resolve_artifact(self, group_id, artifact_id, version, repository, packaging):
+        resolver_url = self._resolver_url(group_id, artifact_id, version, repository, packaging)
+        print resolver_url
+        if self.nexus_auth:
+            resolver_response = requests.get(resolver_url, auth=(self.nexus_user, self.nexus_pass))
+        else:
+            resolver_response = requests.get(resolver_url)
+        if resolver_response.status_code == 200:
+            print resolver_response.content
+        else:
+            print "bad response code: %s" % resolver_response.status_code
+            sys.exit(1)
+        return resolver_response.content
+
+    def _artifact_url(self, artifact_path):
+        return "%s/content/groups/private%s" % (self.nexus_base_url, artifact_path)
+
+    def get_artifact(self, group_id, artifact_id, version="trunk-SNAPSHOT",
+                     repository="snapshots", packaging="war"):
+        artifact_xml = self._resolve_artifact(group_id, artifact_id, version, repository, packaging)
+        parsed_xml = etree.fromstring(artifact_xml)
+
+        artifact_path = parsed_xml.xpath("/artifact-resolution/data/repositoryPath")[0].text
+        artifact_sha1 = parsed_xml.xpath("/artifact-resolution/data/sha1")[0].text
+
+        # do we already have the file, with the requisite sha1 hash?
+        local_artifact_path = os.path.join(self.download_directory, os.path.basename(artifact_path))
+        if os.path.exists(local_artifact_path) and self._hash_file(local_artifact_path) == artifact_sha1:
+            # we've already got the file, don't bother to download
+            return True
+        else:
+            if self.nexus_auth:
+                download_response = requests.get(self._artifact_url(artifact_path),
+                                                 auth=(self.nexus_user, self.nexus_pass), stream=True)
+            else:
+                download_response = requests.get(self._artifact_url(artifact_path), stream=True)
+            with open(local_artifact_path, 'wb') as f:
+                for chunk in download_response.iter_content(chunk_size=4096):
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+                        f.flush()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'nexus',
+  packages = ['nexus'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python client for Nexus.',
+  author = 'Paul Krohn  ',
+  author_email = 'paul@daemonize.com',
+  url = 'https://github.com/paul-krohn/python-nexus', # use the URL to the github repo
+  download_url = 'https://github.com/paul-krohn/python-nexus/tarball/0.1', # I'll explain this in a second
+  keywords = ['nexus', 'fabric'], # arbitrary keywords
+  classifiers = [],
+)
